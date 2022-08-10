f92f99ec32a85db28a6248d787e8f32e5e24cd3e
Anna Crandall <anna.crandall02@gmail.com>
2022-08-09 19:52:26 -0500

    cash register that returns exact change

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,6 +1,5 @@
 function palindrome(str) {
-    const stripped = str.toLowerCase().replace(/\W|_/g, "")
-    console.log(stripped)
+    const stripped = str.toLowerCase().replace(/\W|_/g, ""); 
     const reversed = stripped.split("").reverse().join(''); 
     return stripped === reversed 
     }
