e5a6a65ae7c77211f1764142c1c7c88d95ba4484
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-08-09 19:54:27 -0500

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,6 +1,5 @@
 function palindrome(str) {
     const stripped = str.toLowerCase().replace(/\W|_/g, "")
-    console.log(stripped)
     const reversed = stripped.split("").reverse().join(''); 
     return stripped === reversed 
     }
